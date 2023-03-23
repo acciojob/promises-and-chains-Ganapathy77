e83584187b7e389db5f6d2a1905c0186231bf8df
Ganapathy77 <ganapathy14777@gmail.com>
2023-03-23 23:55:13 +0530

    Completed Coding

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const form = document.querySelector('form');
+const btn = document.querySelector('.btn');
+const nameInput = document.querySelector('#name');
+const ageInput = document.querySelector('#age');
+
+btn.addEventListener('click',(event)=>{
+    event.preventDefault();
+    
+    let name1 = nameInput.value;
+    let age = parseInt(ageInput.value);
+
+    if(isNaN(age) || age<1 || name1.trim() === ""){
+        alert("Please enter a valid input...")
+        return;
+    }
+
+    const promise = new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            if(age>18){
+                resolve();
+            }
+            else{
+                reject();
+            }
+        },4000)
+    }).then(()=>{
+        alert(`Welcome, ${name1}. You can vote.`)
+    }).catch(()=>{
+        alert(`Oh sorry ${name1} You aren't old enough.`)
+    })
+});
+
+
